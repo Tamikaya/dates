3039951bd062496c3d73f4dc6cb4df775409650c
Tamikaya <tamikaya111@gmail.com>
2024-11-14 10:09:11 +0500

    Add files via upload

---
+++ dates.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9**0.5*5)
+# 2nd program
+print(9.99>9.98 and 1000 != 1000.1)
+# 3nd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==2*(2+2))
+# 4nd program
+print('123.456')
+print(123.456 * 10)
+print(int(1234.456))
+print(12+34)
+print(int(46/10))
+print(int(float('123.456')*10)%10)
+
+
+
+
